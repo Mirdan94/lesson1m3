96a9b166353e1cf1c652d7bc82b5d2fdd03f7642
Mirdan Akinov <akinovmirdan@gmail.com>
2019-08-24 15:20:58 +0600

    Changes 1.1

--- app/src/main/java/com/example/lesson1/MainActivity.java
+++ app/src/main/java/com/example/lesson1/MainActivity.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,16 +20,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.text_view);
-        textView.setText(R.);
+
+        final Button equalizer = findViewById(R.id.equalizer);
+        equalizer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String text = editText.getText().toString();
+
+                Log.d("=", text);
+
+                textView.setText(text);
+            }
+        });
 
         final Button one = findViewById(R.id.one);
         one.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("1", "one");
+
+                String one = editText.getText().toString();
+
+                Log.d("1", one);
+
+                textView.setText(one);
             }
         });
 
-        //textView.setText("Hollo nigga");
+
+        editText = findViewById(R.id.edit_text);
+
     }
 }
